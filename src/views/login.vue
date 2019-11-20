998beebd57a682cddecfe663e625e9dc5a09b962
zhangwensi <1248018887@qq.com>
2019-11-20 23:15:23 +0800

    简单编写login页面

---
+++ src/views/login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="login">
+        <div class="navTab">
+            <ul>
+                <li>登录</li>
+                <li>注册</li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+.login {
+    background: #344a5f;;
+    height: 100vh;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,64 @@
+const path = require('path')
+
+module.exports = {  
+  // 基本路径
+  publicPath: process.env.NODE_ENV === 'production' ? '' : '',  
+  // 输出文件目录
+  outputDir: process.env.NODE_ENV === 'production' ? 'dist' : 'devdist',  
+  // eslint-loader 是否在保存的时候检查
+  lintOnSave: false,
+  // 是否使用包含运行时编译器的 Vue 构建版本
+  runtimeCompiler: false,  
+  // 生产环境是否生成 sourceMap 文件
+  productionSourceMap: false,  
+  // 生成的 HTML 中的 <link rel="stylesheet"> 和 <script> 标签上启用 Subresource Integrity (SRI)
+  integrity: false,  
+  // webpack相关配置
+  chainWebpack: (config) => {
+  },
+  configureWebpack: (config) => {    
+  if (process.env.NODE_ENV === 'production') {      
+      // 生产环境
+      config.mode = 'production'
+    } else {      
+      // 开发环境
+      config.mode = 'development'
+    }
+  },  
+  // css相关配置
+  css: {
+    requireModuleExtension: true,
+    // 是否分离css（插件ExtractTextPlugin）
+    extract: true,    
+    // 是否开启 CSS source maps
+    sourceMap: false,   
+    // css预设器配置项
+    loaderOptions: {
+        scss: {
+            prependData: `@import "./src/styles/main.scss";`
+          },
+    },    
+    // 是否启用 CSS modules for all css / pre-processor files.
+    // modules: false
+  },  
+  // 是否使用 thread-loader
+  parallel: require('os').cpus().length > 1, 
+  // PWA 插件相关配置
+  pwa: {}, 
+  // webpack-dev-server 相关配置
+  devServer: {
+    open: false, //编译完是否打开网页
+    host: '0.0.0.0', // 指定使用地址，默认localhost,0.0.0.0代表可以被外界访问
+    port: 8080, // 访问端口
+    https: false,// 编译失败时刷新页面
+    hot: true, // 开启热加载
+    hotOnly: false,// 
+    // http 代理配置
+    proxy: null,
+    before: (app) => {}
+  }, 
+  // 第三方插件配置
+  pluginOptions: {
+
+  }
+}
