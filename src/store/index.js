b48ccc573c4726b4e0816d428f749fb2282ae894
zhangwensi <1248018887@qq.com>
2020-01-05 23:57:35 +0800

    修改nav缩小时的页面css 修改cookie为sessionstorage本地存储

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import Cookie from "cookie_js";
+// import Cookie from "cookie_js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     // 定义首页菜单侧边栏初始展开值
-    isCollapse: JSON.parse(Cookie.get('status')) || false
+    // isCollapse: JSON.parse(Cookie.get('isCollapse'))||false
+    isCollapse: JSON.parse(sessionStorage.getItem('isCollapse'))||false
   },
   // 做属性计算时需要使用getters
   getters: {},
@@ -15,9 +16,12 @@ export default new Vuex.Store({
     SET_COLLOSPE: (state)=> {
       state.isCollapse = !state.isCollapse
       // 解决刷新时的菜单栏状态 可以使用sessionStorage/localStorage/cookie解决
-      Cookie.set('status',JSON.stringify(state.isCollapse))
+      // Cookie.set('isCollapse',JSON.stringify(state.isCollapse))
+      sessionStorage.setItem('isCollapse',JSON.stringify(state.isCollapse))
     }
   },
-  actions: {},
+  actions: {
+    // 处理异步操作，可以通过this.$store.dispatch(content,value) -> Action->mutations达到改变state值
+  },
   modules: {}
 });
